524731b24ad435527ccd0eb8464d343b216db926
Mandine44 <amandine.huret@gmail.com>
2021-12-20 14:40:49 +0100

    commit2

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -4,10 +4,9 @@ package dev;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println ("Hello World!");
     }
 }
