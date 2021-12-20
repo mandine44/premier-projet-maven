5cfafa69924dc7ca88b245f7e0d95710a087d639
Mandine44 <amandine.huret@gmail.com>
2021-12-20 17:20:11 +0100

    tp03mavenProfils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,10 +1,17 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 public class App {
 	public static void main(String[] args) {
-		String asciiArt = FigletFont.convertOneLine("hello");
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
 		System.out.println(asciiArt);
+
+		// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement :" + environnement);
 	}
 }
