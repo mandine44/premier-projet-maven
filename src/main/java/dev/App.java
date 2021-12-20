1b9b9bf9792a376102d92a94101a77fc69866883
Mandine44 <amandine.huret@gmail.com>
2021-12-20 14:47:50 +0100

    commit3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,12 +1,10 @@
 package dev;
 
-/**
- * Hello world!
- *
- */
+import com.github.lalyos.jfiglet.FigletFont;
+
 public class App {
-	public static void main( String[] args )
-    {
-        System.out.println ("Hello World!");
-    }
+	public static void main(String[] args) {
+		String asciiArt = FigletFont.convertOneLine("hello");
+		System.out.println(asciiArt);
+	}
 }
